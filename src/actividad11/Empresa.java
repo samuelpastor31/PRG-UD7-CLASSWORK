40ceb3c940a5fe96208f25aaf5d04cddb51f2e5a
Samuel Pastor <samuelpastorpinillos@gmail.com>
2023-02-15 16:17:04 +0100

    haiendoTestFinal

--- src/actividad11/Empresa.java
+++ src/actividad11/Empresa.java
@@ -1,24 +1,30 @@
 package actividad11;
 
+import actividad11.tiposempleados.Administrativo;
+import actividad11.tiposempleados.Empleado;
+import actividad11.tiposempleados.Vendedor;
+
 public class Empresa {
 
-    private String[] empleados;
+    private Empleado[] empleados;
 
-    public Empresa(String[] empleados) {
-        this.empleados = empleados;
+    public Empresa() {
+        this.empleados = new Empleado[20];
     }
 
     public void anyadirEmpleados(Empleado empleado){
-        for (int i = 0; i <empleados.length ; i++) {
-            if (empleados[i].equals(empleado.dni))
-            {
-                return;
-            }
-            if (empleados[i]==null){
-                empleados[i]=empleado.getDni();
-                if (empleados[i].equals(empleado.dni))
-                {
-                    return;
+        if (empleados.length<=20) {
+            for (int i = 0; i < empleados.length; i++) {
+                if (empleados[i]!=null) {
+                    if (empleados[i].equals(empleado.getDni())) {
+                        return;
+                    }
+                }
+                if (empleados[i] == null) {
+                    empleados[i] = empleado;
+                    if (empleados[i].equals(empleado.getDni())) {
+                        return;
+                    }
                 }
             }
         }
@@ -38,5 +44,54 @@ public class Empresa {
                 System.out.println(empleados[i].toString());
             }
         }
+    }
+
+    public void visualizarListaEmpleados(){
+        System.out.println("------ Todos Los empleados --------");
+        for (int i = 0; i <empleados.length ; i++) {
+            if (empleados[i] != null) {
+                System.out.println(empleados[i].toString());
+            }
+        }
+    }
+
+    public void visualizarListaVendedores(Empleado empleado){
+        for (int i = 0; i <empleados.length ; i++) {
+            if (empleados[i] != null) {
+                if (empleado instanceof Vendedor) {
+                    System.out.println(empleados[i].toString());
+                }
+            }
+        }
+    }
+
+    public void visualizarListaAdministrativos(Empleado empleado){
+        for (int i = 0; i <empleados.length ; i++) {
+            if (empleados[i] != null) {
+                if (empleado instanceof Administrativo) {
+                    System.out.println(empleados[i].toString());
+                }
+            }
+        }
+    }
+
+    public double totalMensualSalarios(){
+        double totalSalariosMenusal = 0;
+        for (int i = 0; i <empleados.length ; i++) {
+            if (empleados[i] != null) {
+                totalSalariosMenusal = totalSalariosMenusal + empleados[i].getSalario();
+            }
+        }
+
+        return totalSalariosMenusal;
+    }
+
+    public void aumentoSalarioGeneralizado(){
+        for (int i = 0; i < empleados.length ; i++) {
+            if (empleados[i] != null) {
+                empleados[i].incrementarSalario();
+            }
+        }
+        System.out.println("------ Aumento salario de empleados ------");
     }
 }
